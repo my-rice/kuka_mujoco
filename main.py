50478b00155a0f566835497e864d1964c94bfc3d
my-rice <dvdrsi2017@gmail.com>
2024-08-23 11:44:06 +0200

    [ADD] qpos tracking is now working

--- main.py
+++ main.py
@@ -7,109 +7,42 @@ from trajectory_interpolator import TrajectoryInterpolator
 
 import os
 import imageio
+import copy
+
+from mujoco_mpc import agent as agent_lib
 
 PATH_TO_MODEL = "kuka_iiwa_14/scene.xml"
 VIDEO_DIR = "results/"
 DATA_DIR = "results/"
 
 
-# Load the model
-model = mujoco.MjModel.from_xml_path(PATH_TO_MODEL)
-data = mujoco.MjData(model)
+def run():
+    # Load the model
+    model = mujoco.MjModel.from_xml_path(PATH_TO_MODEL)
+    data = mujoco.MjData(model)
 
-renderer = mujoco.Renderer(model, 480, 640)
-
-
-duration = 5  # (seconds)
-framerate = 30  # (Hz)
-n_steps = int(np.ceil(duration * framerate)) + 1
-
-# Simulate and display video.
-frames = []
-mujoco.mj_resetData(model, data)  # Reset state and time.
+    renderer = mujoco.Renderer(model, 480, 640)
 
 
-# Create a logger
-header = ["time"]
-header += [f"qpos_{i}" for i in range(model.nq)]
-header += [f"qvel_{i}" for i in range(model.nv)]
-header += [f"qacc_{i}" for i in range(model.nv)]
-header += [f"ctrl_{i}" for i in range(model.na)]
+    duration = 5  # (seconds)
+    framerate = 30  # (Hz)
+    n_steps = int(np.ceil(duration * framerate)) + 1
 
-logger = Logger(DATA_DIR + "log.csv", header)
-# Log the initial state
-
-message = {
-    "time": data.time,
-    **{
-        f"qpos_{i}": data.qpos[i] for i in range(model.nq)
-    },
-    **{
-        f"qvel_{i}": data.qvel[i] for i in range(model.nv)
-    },
-    **{
-        f"qacc_{i}": data.qacc[i] for i in range(model.nv)
-    },
-    **{
-        f"ctrl_{i}": data.ctrl[i] for i in range(model.na)
-    },
-}
-
-logger.log(message)
-
-
-# Instantiate the interpolator
-target_q = np.array(
-    [
-        0.28932849,
-        0.92062463,
-        -0.9442432,
-        -1.02045466,
-        1.2824041,
-        -0.40766866,
-        -1.41391154,
-    ]
-)
-
-qpos_start = data.qpos.copy()
-qpos_end = target_q
-
-trajectory_interpolator = TrajectoryInterpolator(qpos_start, np.zeros(7), 0.5, qpos_end, np.zeros(7), time_step=model.opt.timestep)
-
-#model.opt.disableflags = 1 << 4 # disable contact constraints
-
-
-while data.time < duration:
-    
-    # compute desired acceleration
-    t = data.time
-    error = target_q - data.qpos
+    # Simulate and display video.
+    frames = []
+    mujoco.mj_resetData(model, data)  # Reset state and time.
 
     
-    prev_acc = data.qacc.copy()
+    # Create a logger
+    header = ["time"]
+    header += [f"qpos_{i}" for i in range(model.nq)]
+    header += [f"qvel_{i}" for i in range(model.nv)]
+    header += [f"qacc_{i}" for i in range(model.nv)]
+    header += [f"ctrl_{i}" for i in range(model.na)]
 
-    # inverse dynamics to compute required torque.
-    # set qacc to the desired acceleration
-    target_acc = trajectory_interpolator.get_acc(data.qpos, data.qvel, t)
-    #target_acc = trajectory_interpolator.get_acc_by_derivative(data.qpos, data.qvel, t)
-    data.qacc[:] = target_acc
+    logger = Logger(DATA_DIR + "log.csv", header)
+    # Log the initial state
 
-    mujoco.mj_inverse(model, data)
-
-    #print("data.solver_fwdinv", data.solver_fwdinv) # See if the inverse dynamics will be equal to the computed torque
-    data.qacc[:] = prev_acc
-
-    # set torque as control
-    data.ctrl[:] = data.qfrc_inverse.copy()
-
-    mujoco.mj_step(model, data)
-
-    if len(frames) < data.time * framerate:
-        renderer.update_scene(data, scene_option=dict())
-        pixels = renderer.render()
-        frames.append(pixels)
-    
-    
     message = {
         "time": data.time,
         **{
@@ -129,9 +62,90 @@ while data.time < duration:
     logger.log(message)
 
 
-if renderer is not None:
-    renderer.close()
+    # Instantiate the interpolator
+    target_q = np.array(
+        [
+            0.28932849,
+            0.92062463,
+            -0.9442432,
+            -1.02045466,
+            1.2824041,
+            -0.40766866,
+            -1.41391154,
+        ]
+    )
 
-imageio.mimsave(os.path.join(VIDEO_DIR, "video.mp4"), frames, fps=framerate)
-#logger.save()
-logger.plot_columns("results/qpos.png", columns_names=[f"qpos_{i}" for i in range(model.nq)], references = [target_q[i] for i in range(model.nq)])
+    qpos_start = data.qpos.copy()
+    qpos_end = target_q
+
+    target_vel = np.zeros(7)
+    trajectory_interpolator = TrajectoryInterpolator(qpos_start, np.zeros(7), 5, qpos_end, target_vel, time_step=model.opt.timestep)
+
+    #model.opt.disableflags = 1 << 4 # disable contact constraints
+
+
+
+    while data.time < duration:
+        
+        # compute desired acceleration
+        t = data.time
+        prev_acc = data.qacc.copy()
+        
+        # q_step_t = trajectory_interpolator.get_acc(data.qpos, data.qvel, t)
+        # vel = np.zeros(7)
+        # mujoco.mj_differentiatePos(model, vel, model.opt.timestep ,data.qpos, q_step_t)
+        # target_acc = np.zeros(7)
+        # mujoco.mj_differentiatePos(model, target_acc,model.opt.timestep,data.qvel, vel)
+                
+        target_acc = trajectory_interpolator.get_acc(data.qpos, data.qvel, t)
+
+
+        data.qacc[:] = target_acc
+        mujoco.mj_inverse(model, data)
+        # set torque as control
+        # print ctrl range
+        print("ctrl range", model.actuator_ctrlrange)
+
+        data.ctrl[:] = data.qfrc_inverse.copy()
+        #print("data.solver_fwdinv", data.solver_fwdinv) # See if the inverse dynamics will be equal to the computed torque
+        data.qacc[:] = prev_acc
+        mujoco.mj_step(model, data)
+
+
+
+        if len(frames) < data.time * framerate:
+            renderer.update_scene(data, scene_option=dict())
+            pixels = renderer.render()
+            frames.append(pixels)
+        
+        
+        message = {
+            "time": data.time,
+            **{
+                f"qpos_{i}": data.qpos[i] for i in range(model.nq)
+            },
+            **{
+                f"qvel_{i}": data.qvel[i] for i in range(model.nv)
+            },
+            **{
+                f"qacc_{i}": data.qacc[i] for i in range(model.nv)
+            },
+            **{
+                f"ctrl_{i}": data.ctrl[i] for i in range(model.na)
+            },
+        }
+
+        logger.log(message)
+
+
+    if renderer is not None:
+        renderer.close()
+
+    imageio.mimsave(os.path.join(VIDEO_DIR, "video.mp4"), frames, fps=framerate)
+    #logger.save()
+    logger.plot_columns("results/qpos.png", columns_names=[f"qpos_{i}" for i in range(model.nq)], references = [target_q[i] for i in range(model.nq)])
+
+
+
+if __name__ == "__main__":
+    run()
--- trajectory_interpolator.py
+++ trajectory_interpolator.py
@@ -24,35 +24,62 @@ class TrajectoryInterpolator:
         # Create the cubic Hermite spline (Cubic Hermite is good for qpos and qvel)
         self.spline = CubicHermiteSpline(times, positions, velocities)
 
+        # Setup the interpolator
+        self.setup()
 
-    def get_acc_by_derivative(self, qpos, qvel, t):
-        """Compute the desired acceleration at time t"""
+    # def get_acc_by_derivative(self, qpos, qvel, t):
+    #     """Compute the desired acceleration at time t"""
 
-        pos_error = self.final_qpos - qpos
-        vel_error = self.final_qvel - qvel
+    #     pos_error = self.final_qpos - qpos
+    #     vel_error = self.final_qvel - qvel
 
-        # Use the derivative of the error to compute the desired acceleration
-        desired_acc = (pos_error/self.time_step - qvel)/self.time_step
+    #     # Use the derivative of the error to compute the desired acceleration
+    #     desired_acc = (pos_error/self.time_step - qvel)/self.time_step
 
-        return desired_acc
+    #     return desired_acc
+
+
+
+    def setup(self):
+        self.a0 = self.starting_qpos
+        self.a1 = self.starting_qvel
+        self.a2 = (3*(self.final_qpos - self.starting_qpos) - (2*self.starting_qvel + self.final_qvel)*self.duration) / (self.duration**2)
+        self.a3 = (2*(self.starting_qpos - self.final_qpos) + (self.starting_qvel + self.final_qvel)*self.duration) / (self.duration**3)
 
 
     def get_acc(self,qpos, qvel, t):
-        """Compute the desired acceleration at time t"""
+        """Compute the desired position at time t"""
 
         if t > self.duration:
-            return np.zeros_like(qvel)
-        # Use spline interpolation
-        # Compute the desired acceleration at time t
+            return np.zeros(7)
+
+        return 6*self.a3*t + 2*self.a2
+    
+    def get_pos(self, t):
+        """Compute the desired position at time t"""
+
+        if t > self.duration:
+            return self.final_qpos
+
+        return self.a0 + self.a1*t + self.a2*t**2 + self.a3*t**3
         
-        # Create the spline
-        times = np.array([t, self.duration])  # start and end times
-        positions = np.array([self.starting_qpos, self.final_qpos])
-        velocities = np.array([self.starting_qvel, self.final_qvel])
-
-        # Create the cubic Hermite spline (Cubic Hermite is good for qpos and qvel)
-        self.spline = CubicHermiteSpline(times, positions, velocities)
 
 
+if __name__ == "__main__":
 
-        return self.spline(t, 2)  # 2nd derivative is acceleration
+    qpos_start = np.array([0,0])
+    qpos_end = np.array([1,1])
+
+    qvel_start = np.array([0,0])
+    qvel_end = np.array([0,0])
+
+    trajectory_interpolator = TrajectoryInterpolator(qpos_start, qvel_start, 1, qpos_end, qvel_end)
+
+    print(trajectory_interpolator.get_pos(0.5))
+    print(trajectory_interpolator.get_pos(1.0))
+    print(trajectory_interpolator.get_pos(1.5))
+
+    print(trajectory_interpolator.get_acc(qpos_start, qvel_start, 0.5))
+    print(trajectory_interpolator.get_acc(qpos_start, qvel_start, 1.0))
+    print(trajectory_interpolator.get_acc(qpos_start, qvel_start, 1.5))
+    
